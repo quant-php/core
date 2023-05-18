828e2d62d7a65302b3c56fcb78b41e2d951abb35
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-05-18 19:48:03 +0200

    refactor: update quant/core

---
+++ Tests/Attribute/SetterTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the quant project.
+ *
+ * (c) 2023 Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
+ *
+ * For full copyright and license information, please consult the LICENSE-file distributed
+ * with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Quant\Core\Tests\Attribute;
+
+use PHPUnit\Framework\TestCase;
+use Quant\Core\Attribute\Accessor;
+use Quant\Core\Attribute\Setter;
+
+class SetterTest extends TestCase
+{
+    public function testSetter(): void
+    {
+        $this->assertInstanceOf(Accessor::class, new Setter());
+    }
+}
